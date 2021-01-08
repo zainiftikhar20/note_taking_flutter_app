7cb4cfaf9810e71c33d8dae9dddfa25a185fce31
zainiftikhar20 <71585418+zainiftikhar20@users.noreply.github.com>
2021-01-09 00:36:23 +0500

    App Icon Changed

--- lib/Screen_Folder/Note_Edit_Screen.dart
+++ lib/Screen_Folder/Note_Edit_Screen.dart
@@ -30,8 +30,8 @@ class _Note_Edit_ScreenState extends State<Note_Edit_Screen> {
   int id;
   FloatingActionButtonLocation _fabLocation =
       FloatingActionButtonLocation.endDocked;
-  bool _isBottomBarNotched = false;
-  bool _isFabMini = false;
+  // bool _isBottomBarNotched = false;
+  // bool _isFabMini = false;
 
   @override
   void didChangeDependencies() {
